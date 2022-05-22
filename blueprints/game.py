e772bb1e51e72b6f5b72678e70dfe3a11078ea4f
Hang <747296683@qq.com>
2022-05-22 16:50:12 +0800

    Remove deprecated url

--- blueprints/game.py
+++ blueprints/game.py
@@ -7,7 +7,6 @@ bp = Blueprint("game", __name__, url_prefix="")
 @bp.route("/play")
 @login_required
 def play():
-
     return render_template('game_play.html')
 
 
@@ -15,13 +14,3 @@ def play():
 def intro():
     return render_template('intro.html')
 
-
-@bp.route("/tips")
-def tips():
-    return render_template('index.html')
-    # return render_template('index.html')
-
-
-@bp.route("/marks")
-def marks():
-    return render_template('marks.html')
